58e72782450361ae7426532f97dadd757fd4db45
Marvin Guelzow <marvinguelzow@googlemail.com>
2015-02-28 19:01:59 +0100

    can now access multiple models in template, added todo-type dropdown on website

--- todolist/views.py
+++ todolist/views.py
@@ -7,12 +7,19 @@ from todolist.models import *
 
 class TodoListView(generic.ListView):
     template_name = 'todolist/index.html'
-    context_object_name = 'todo_item_list'
+    context_object_name = 'todo_data'
+
+    def get_context_data(self, **kwargs):
+        context = super(generic.ListView, self).get_context_data(**kwargs)
+        context['todo_items'] = TodoItem.objects.all()
+        context['todo_types'] = TodoType.objects.all()
+        return context
 
     def get_queryset(self):
         """Return all todo items."""
         return TodoItem.objects.all()
 
+    
 #def add_todo(request, new_todo_text):
 def add_todo(request):
     html = "<html><body>Got something:</br>POST: " + request.POST.get("new_todo_text", "") + "</body></html>"
